999aa189b939a33794d15500f21fed4ed3cd1a8b
Nathan Toups <n@rjrbt.io>
2026-04-06 23:21:37 -0600

    fix: change background color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" data-theme="dark">
-      <body className="min-h-screen">{children}</body>
+      <body className="min-h-screen bg-black">{children}</body>
     </html>
   );
 }
